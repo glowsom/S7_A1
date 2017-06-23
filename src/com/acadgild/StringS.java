9096cd5e97fb50c7d3a6d7bd210ca2697d8a4e32
Samuel O. Appiah <me@samobeng.com>
2017-06-23 01:12:17 -0500

    Initial Commit

---
+++ src/com/acadgild/StringS.java
@@ -0,0 +1,22 @@
+package com.acadgild;
+
+public class StringS {
+
+	public static void main(String[] args) {
+		/*
+		 *  TODO Write a program to convert an int variable to String
+		 *  using an inbuilt function of String class.
+		 */
+		
+		//Declaring and initializing int variable: var 
+		int var = 6;
+		
+		//Using an inbuilt function of String class to convert var to String.
+		String varString = String.valueOf(var);
+		
+		//Displaying int converted to String
+		System.out.println(varString);
+
+	}
+
+}
